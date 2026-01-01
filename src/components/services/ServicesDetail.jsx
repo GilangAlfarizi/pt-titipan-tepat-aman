941d06a76c7673cf25d260eb572438a4e4d79803
GilangAlfarizi <alfarizigilang666@gmail.com>
2026-01-01 14:19:21 +0700

    feat: make service page responsive

--- src/components/services/ServicesDetail.jsx
+++ src/components/services/ServicesDetail.jsx
@@ -1,17 +1,17 @@
 export const ServicesDetail = ({ imgSrc, imgAlt, title, description }) => {
 	return (
-		<section className="mx-auto max-w-screen-xl lg:pt-8 lg:mt-8 bg-neutral-primary">
-			<div className="bg-neutral-secondary-soft border border-default rounded-base p-8 md:p-12 flex items-start">
-				<div className="me-8">
+		<section className="mx-auto max-w-7xl lg:pt-8 lg:mt-8 mt-4 bg-neutral-primary">
+			<div className="bg-neutral-secondary-soft border border-default rounded-base p-8 mx-8 md:p-12 lg:flex items-start">
+				<div className="lg:me-8 mx-auto">
 					<img
 						src={imgSrc}
 						alt={imgAlt}
-						className="max-w-2xs rounded object-cover"
+						className="lg:max-w-2xs rounded object-cover"
 					/>
 				</div>
 
 				<div className="flex-1">
-					<h1 className="text-heading tracking-tighter text-3xl font-bold mb-6">
+					<h1 className="text-heading tracking-tighter text-3xl font-bold lg:mb-6 mb-4 lg:mt-0 mt-4">
 						{title}
 					</h1>
 					<p className="md:text-lg font-normal text-body">{description}</p>
